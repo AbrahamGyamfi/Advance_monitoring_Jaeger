2ee4450aa7b557b79e56fa4fa26bfd31730c5bd5
AbrahamGyamfi <gyamfiabraham95@email.com>
2026-02-28 21:50:05 +0000

    fix: Use monitoring server private IP for Jaeger OTLP endpoint
    - Dynamically fetch monitoring server private IP from AWS
    - Fixes Jaeger trace collection issue (Docker bridge network blocks public IP)
    - Ensures traces are properly sent to Jaeger on port 4318

--- hooks/start.sh
+++ hooks/start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd /home/ec2-user/taskflow
+
+# Get monitoring server private IP from AWS
+REGION=$(ec2-metadata --availability-zone | cut -d " " -f 2 | sed 's/[a-z]$//')
+MONITORING_PRIVATE_IP=$(aws ec2 describe-instances --region $REGION --filters "Name=tag:Name,Values=TaskFlow-Monitoring-Server" "Name=instance-state-name,Values=running" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 697863031884.dkr.ecr.eu-west-1.amazonaws.com
 export REGISTRY_URL=697863031884.dkr.ecr.eu-west-1.amazonaws.com
 export IMAGE_TAG=latest
-export MONITORING_HOST=54.78.155.203
+export MONITORING_HOST=${MONITORING_PRIVATE_IP:-localhost}
 docker-compose up -d
