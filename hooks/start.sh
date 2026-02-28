aa24cd1bca1f7f18a51cd8a13a279ff815f2cc7a
AbrahamGyamfi <gyamfiabraham95@email.com>
2026-02-28 18:35:58 +0000

    Set env vars in start script

--- hooks/start.sh
+++ hooks/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/taskflow
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 697863031884.dkr.ecr.eu-west-1.amazonaws.com
+export REGISTRY_URL=697863031884.dkr.ecr.eu-west-1.amazonaws.com
+export IMAGE_TAG=latest
+export MONITORING_HOST=54.78.155.203
 docker-compose up -d
