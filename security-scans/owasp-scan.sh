098c7e8b6a1b99517e94740f42b5d33d87772970
AbrahamGyamfi <gyamfiabraham95@email.com>
2026-03-01 15:24:22 +0000

    remove owasp fail flag

--- security-scans/owasp-scan.sh
+++ security-scans/owasp-scan.sh
@@ -17,8 +17,7 @@ docker run --rm -v $(pwd):/src -v ~/.m2:/root/.m2 \
     --format HTML \
     --out /src \
     --project "$DIR" \
-    --nvdApiKey "${NVD_API_KEY}" \
-    --failOnCVSS 7
+    --nvdApiKey "${NVD_API_KEY}"
 
 # Move reports
 mv dependency-check-report.json ../owasp-$DIR-report.json 2>/dev/null || true
